b02153c46f706249aa9320c47da9fb3598897c86
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-02-05 08:21:12 +0000

    next test, red

--- sum-of-digits.test.js
+++ sum-of-digits.test.js
@@ -6,3 +6,8 @@ it('should split the numbers up', () => {
   const sumOfNumbersFunc = sumOfNumbers(123);
   expect(sumOfNumbersFunc).toEqual(1,2,3);
 });
+
+it('should add the numbers up', () => {
+  const sumOfNumbersFunc = sumOfNumbers(123);
+  expect(sumOfNumbersFunc).toEqual(6);
+});
