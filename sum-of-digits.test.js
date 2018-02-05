0ce2b2a550094d3ef99304cf6e53e820520d4340
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-02-05 08:55:15 +0000

    refactor of function for first test - was not working...

--- sum-of-digits.test.js
+++ sum-of-digits.test.js
@@ -1,10 +1,15 @@
 function sumOfNumbers(num) {
-  return myThing = parseInt(String(num).split('').join(','));
+  
+  var digits = num.toString().split('').map(function(item, index) {
+    return parseInt(item);
+  });
+
+  return digits;
 };
 
 it('should split the numbers up', () => {
   const sumOfNumbersFunc = sumOfNumbers(123);
-  expect(sumOfNumbersFunc).toEqual(1,2,3);
+  expect(sumOfNumbersFunc).toEqual([1,2,3]);
 });
 
 it('should add the numbers up', () => {
